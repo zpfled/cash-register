239d80319bc372575f8858925064f5916a5d191d
zachpflederer <zach@fastmodeltechnologies.com>
2017-03-01 12:41:55 -0600

    test: add test cases from challenge

--- test/CashRegisterSpec.js
+++ test/CashRegisterSpec.js
@@ -3,7 +3,29 @@ import { describe, it } from 'mocha';
 import expect from 'expect';
 
 describe('CashRegister', () => {
-  it('is true', () => {
-    expect(true).toEqual(true);
+  const cr = new CashRegister();
+
+  it('represent current moneys as a string', () => {
+    expect(cr.toString()).toEqual('$0 0x20 0x10 0x5 0x2 0x1');
+  });
+
+  it('add bills in each denomination: #$20 #$10 #$5 #$2 #$1', () => {
+    cr.add([ 2, 4, 6, 4, 10 ]);
+    expect(cr.toString()).toEqual('$128 2x20 4x10 6x5 4x2 10x1');
+  });
+
+  it('take bills in each denomination: #$20 #$10 #$5 #$2 #$1', () => {
+    cr.take([ 1, 4, 3, 0, 10 ]);
+    expect(cr.toString()).toEqual('$43 1x20 0x10 3x5 4x2 0x1');
+  });
+
+  it('given a dollar amount, remove money from cash register to make change', () => {
+    cr.change(11);
+    expect(cr.change(11)).toEqual('0x20 0x10 1x5 3x2 0x1');
+    expect(cr.toString()).toEqual('$32 1x20 0x10 2x5 1x2 0x1');
+  });
+
+  it('raise error if change cannot be made', () => {
+    expect(cr.change(14)).toEqual('sorry');
   });
 });
